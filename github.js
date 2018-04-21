4219cce41fbcb5b966b9199ed855579c0f1ac68b
Akshay Choudhary <ak92choudhary@gmail.com>
2018-04-20 22:02:21 -0400

    fixed ui

--- github.js
+++ github.js
@@ -8,7 +8,7 @@ class Github {
     async getUser(user){
         const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profileDate = await profileResponse.json();
+        const profile = await profileResponse.json();
 
         return {
             profile
